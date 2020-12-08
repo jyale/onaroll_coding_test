4f48b0407d61f1744f95675a150a084d1387c013
John M <john@noreply>
2020-12-07 21:39:58 -0500

    updates

--- fetch_reddit_tests.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import praw, pickle
-import os.path
-from os import path
-
-
-# make several dictionries and save them to disk
-# call the compare dicts fundtion and check the results
